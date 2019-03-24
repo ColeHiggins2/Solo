5c3a32a2b370ba6fb8a2917d50e3399ffa54827c
colehiggins2 <colehiggins2@gmail.com>
2019-03-24 12:12:07 -0400

    test colors

--- solo/solo/urls.py
+++ solo/solo/urls.py
@@ -26,6 +26,7 @@ urlpatterns = [
     path('thanks/',views.ThanksPage.as_view(),name='thanks'),
     path('posts/', include("posts.urls", namespace="posts")),
     path('groups/',include("groups.urls", namespace="groups")),
+    path('buildings/',views.BuildingsPage.as_view(),name='buildings'),
 
 
 ]
