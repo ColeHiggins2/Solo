58f49646b0587fcf5400ea39f8adbadfc7367273
colehiggins2 <colehiggins2@gmail.com>
2019-04-07 14:47:21 -0400

    settings import heroku

--- solo/solo/settings.py
+++ solo/solo/settings.py
@@ -13,7 +13,7 @@ https://docs.djangoproject.com/en/2.1/ref/settings/
 
 import os
 
-#import django_heroku
+
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -135,6 +135,9 @@ MEDIA_URL = '/media/'
 
 LOGIN_REDIRECT_URL = 'test'
 LOGOUT_REDIRECT_URL = 'thanks'
+
+import django_heroku
+django_heroku.settings(locals())
 
 # Activate Django-Heroku.
 #django_heroku.settings(locals())
