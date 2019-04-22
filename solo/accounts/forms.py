e305312ad96c2fd5bde01f9f36b6b5b89528e01e
sgreth <sgreth@uncc.edu>
2019-04-22 14:28:27 -0400

    Created and populated survey and profile questions

--- solo/accounts/forms.py
+++ solo/accounts/forms.py
@@ -19,19 +19,3 @@ class UserCreateForm(UserCreationForm):
         self.fields['email'].label = 'Email Address'
         self.fields['first_name'].label = 'First Name'
         self.fields['last_name'].label = 'Last Name'
-
-
-
-"""class UserSurveyForm(forms.Form):
-
-    class Meta:
-        fields = ('username', 'email','first_name', 'last_name', 'password1', 'password2')
-        model = get_user_model()
-
-    def __init__(self,*args,**kwargs):
-        super().__init__(*args,**kwargs)
-        self.fields['username'].label = 'Display Name'
-        self.fields['email'].label = 'Email Address'
-        self.fields['first_name'].label = 'First Name'
-        self.fields['last_name'].label = 'Last Name'
-        self.fields['favorite'].label = 'My Favorites'"""
